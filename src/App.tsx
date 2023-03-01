2e3c95fc777bfe5ca05735650a24ba7a3e8d0039
vindika-teaminvos <126548734+vindika-teaminvos@users.noreply.github.com>
2023-03-01 10:56:57 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import Icon from "./assets/AcademicCap.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex-container-left">
+        <div>
+          <svg className="img" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"></path>
+          </svg>
+        </div>
+
+        <div className="button-panel">
+          <button>
+            {/* <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
+            </svg> */}
+            Dashboard</button>
+          <button>Team</button>
+          <button>Projects</button>
+          <button>Calendar</button>
+          <button>Documents</button>
+          <button>Reports</button>
+        </div>
+      </div>
+
+      <div className="flex-container-right">
+        <div className="flex-container-search">
+          <input className="search" type="search" placeholder="Search">
+
+          </input>
+          <div>
+            <svg className="img" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"></path>
+            </svg>
+          </div>
+        </div>
+
+
+        <h2 className="header">
+          Dashboard
+          <div className="box">
+
+          </div>
+        </h2>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
+
